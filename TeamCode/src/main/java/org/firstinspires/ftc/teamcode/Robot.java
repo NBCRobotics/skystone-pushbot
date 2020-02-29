f01117d1c02f279130165104d5abb8144db3d82f
Pranav Avva <pranav.avva@gmail.com>
2020-02-29 14:11:16 -0500

    more stuff and things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,4 +1,55 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Robot {
+
+    private DcMotorEx leftDrive;
+    private DcMotorEx rightDrive;
+
+    private DcMotorEx[] motors;
+
+    public Robot(HardwareMap hardwareMap) {
+        this.leftDrive = hardwareMap.get(DcMotorEx.class, "leftDrive");
+        this.rightDrive = hardwareMap.get(DcMotorEx.class, "rightDrive");
+
+        this.motors = new DcMotorEx[]{leftDrive, rightDrive};
+
+        for (DcMotorEx motor : motors) {
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            motor.setPower(0);
+        }
+
+        this.leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    public void drive(double leftPower, double rightPower) {
+        this.leftDrive.setPower(leftPower);
+        this.rightDrive.setPower(rightPower);
+    }
+
+    public void drive(double power) {
+        this.drive(power, power);
+    }
+
+    public double getLeftDrivePower() {
+        return this.leftDrive.getPower();
+    }
+
+    public double getRightDrivePower() {
+        return this.rightDrive.getPower();
+    }
+
+    public int getLeftDrivePosition() {
+        return this.leftDrive.getCurrentPosition();
+    }
+
+    public int getRightDrivePosition() {
+        return this.rightDrive.getCurrentPosition();
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleOpDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleOpDrive.java
@@ -1,6 +1,49 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Robot;
+
+@TeleOp
 public class TeleOpDrive extends OpMode {
+
+    private Robot robot;
+    private GamepadEx gamepadOne;
+    private GamepadEx gamepadTwo;
+
+    /**
+     * User defined init method
+     * <p>
+     * This method will be called once when the INIT button is pressed.
+     */
+    @Override
+    public void init() {
+        robot = new Robot(hardwareMap);
+        this.gamepadOne = new GamepadEx(gamepad1);
+        this.gamepadTwo = new GamepadEx(gamepad2);
+    }
+
+    /**
+     * User defined loop method
+     * <p>
+     * This method will be called repeatedly in a loop while this op mode is running
+     */
+    @Override
+    public void loop() {
+        robot.drive(-gamepadOne.getLeftY(), -gamepadOne.getRightY());
+
+        telemetry.addData("Status: ", "Running...");
+        telemetry.addLine();
+
+        telemetry.addData("Left Motor Power: ", this.robot.getLeftDrivePower());
+        telemetry.addData("Right Motor Power: ", this.robot.getRightDrivePower());
+        telemetry.addLine();
+
+        telemetry.addData("Left Motor Position: ", this.robot.getLeftDrivePosition());
+        telemetry.addData("Right Motor Position: ", this.robot.getRightDrivePosition());
+
+        telemetry.update();
+    }
 }
